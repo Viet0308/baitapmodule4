e7d9a16fbed5d9db995cccbd7724be4fe9a96bb3
Viet0308 <phamviet0308@gmail.com>
2022-11-16 09:18:09 +0700

    up bai tap

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -6,13 +6,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductService {
-    public List<Product> products = new ArrayList<>();
+    public static List<Product> products = new ArrayList<>();
 
-    public void add(Product product){
+    static {
+        products.add(new Product(1,"Lộc","https://thethaovanhoa.mediacdn.vn/Upload/BLtvcXjb72tSqs1jiHr8g/files/2018/12/Jungkook13.jpg", 50000.0));
+        products.add(new Product(2,"Khánh","https://img.meta.com.vn/Data/image/2022/02/17/anh-jungkook-1.jpg", 30000.0));
+        products.add(new Product(3,"Mạnh","https://i.pinimg.com/originals/06/91/15/069115aea5fc95a5548b4562da55c680.jpg", 40000.0));
+        products.add(new Product(4,"Vân","https://i.pinimg.com/originals/af/87/5c/af875cab80721bb5ee5e5143bc46a998.png", 20000.0));
+    }
+
+    public static void add(Product product){
         products.add(product);
     }
 
-    public void edit(int id, Product product){
+    public static void edit(int id, Product product){
         int index = findIndexByID(id);
         if (index != -1){
             products.set(index,product);
@@ -20,7 +27,7 @@ public class ProductService {
 
     }
 
-    public int findIndexByID(int id){
+    public static int findIndexByID(int id){
         for (int i=0; i<products.size();i++){
             if ( products.get(i).getId() == id ){
                 return i;
@@ -29,7 +36,7 @@ public class ProductService {
         return -1;
     }
 
-    public void delete(int id){
+    public static void delete(int id){
         int index = findIndexByID(id);
         if (index !=-1){
             products.remove(index);
